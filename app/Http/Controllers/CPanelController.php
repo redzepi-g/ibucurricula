b8722e2cb3d2e607e241ea40701ee20ca68d58dd
redzepi-g <gazmendredzepi94@gmail.com>
2018-04-29 01:07:50 +0200

    excel

--- app/Http/Controllers/CPanelController.php
+++ app/Http/Controllers/CPanelController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+
+
 use App\Department;
 use App\Professor;
 use App\Assistant;
@@ -93,7 +99,90 @@ class CPanelController extends Controller
 
         $departmentId = $request->department;
 
-        echo "I am STAFF" .$departmentId;
+        $department = Department::find($departmentId);
+
+        $first_fall = DB::table('ce_first_fall')->get();
+        $first_spring = DB::table('ce_first_spring')->get();
+        $count = count($first_fall);
+        $count_first_spring = count($first_spring);
+
+        $lastrow = 0;
+      
+
+        // Create new Spreadsheet object
+        $spreadsheet = new Spreadsheet();
+
+        // Set document properties
+        $spreadsheet->getProperties()->setCreator('IBU Office')
+            ->setLastModifiedBy('IBU Office')
+            ->setTitle('Office 2007 XLSX Test Document')
+            ->setSubject('Office 2007 XLSX Test Document')
+            ->setDescription('Test document for Office 2007 XLSX, generated using PHP classes.')
+            ->setKeywords('office 2007 openxml php')
+            ->setCategory('Test result file');
+
+        for($i = 3; $i<=$count+2; $i++){
+
+        // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A1' , '#')
+        ->setCellValue('B1', 'Code')
+        ->setCellValue('C1', 'Course')
+        ->setCellValue('D1', 'Weekly Hours')
+        ->setCellValue('E1', 'ECTS')
+        ->setCellValue('F1', 'Professor')
+        ->setCellValue('G1', 'Assistant')
+        ->setCellValue('H1','Nr of Students')
+        ->setCellValue('I1','Comments')
+        ->setCellValue('A2', '1st Year - Fall Semester')
+        ->setCellValue('A'.$i, $first_fall[$i-3]->id)
+        ->setCellValue('B'.$i, $first_fall[$i-3]->code)
+        ->setCellValue('C'.$i, $first_fall[$i-3]->course)
+        ->setCellValue('D'.$i, $first_fall[$i-3]->weeklyhours)
+        ->setCellValue('E'.$i, $first_fall[$i-3]->ects)
+        ->setCellValue('F'.$i, $first_fall[$i-3]->professor)
+        ->setCellValue('G'.$i, $first_fall[$i-3]->assistant)
+        ->setCellValue('H'.$i, $first_fall[$i-3]->students)
+        ->setCellValue('I'.$i, $first_fall[$i-3]->comment);
+        
+        $lastrow = $i;
+        }
+
+        $lastrow++;
+  
+                 //9                   //6                 //9
+        for($i = $lastrow; $i< 16; $i++){
+            
+            $c= ($i - $lastrow)-1;
+            
+ 
+            // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow , '1st Year - Spring Semester')
+        ->setCellValue('A'.$i, $first_spring[$c]->id);
+            
+        }
+
+        // Rename worksheet
+        $spreadsheet->getActiveSheet()->setTitle('Simple');
+
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $spreadsheet->setActiveSheetIndex(0);
+
+        // Redirect output to a client’s web browser (Xlsx)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="01simple.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('php://output');
+        exit;
+
+
+       
     }
 
 

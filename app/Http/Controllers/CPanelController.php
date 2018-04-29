5212d2afede3a7bb754550eecbec082889ce6c77
Redzepi <gazmendredzepi94@gmail.com>
2018-04-29 15:13:57 +0200

    excel complete

--- app/Http/Controllers/CPanelController.php
+++ app/Http/Controllers/CPanelController.php
@@ -103,10 +103,25 @@ class CPanelController extends Controller
 
         $first_fall = DB::table('ce_first_fall')->get();
         $first_spring = DB::table('ce_first_spring')->get();
+        $second_fall = DB::table('ce_second_fall')->get();
+        $second_spring = DB::table('ce_second_spring')->get();
+        $third_fall = DB::table('ce_third_fall')->get();
+        $third_spring = DB::table('ce_third_spring')->get();
+        $fourth_fall = DB::table('ce_fourth_fall')->get();
+        $fourth_spring = DB::table('ce_fourth_spring')->get();
+
         $count = count($first_fall);
         $count_first_spring = count($first_spring);
+        $count_second_fall = count($second_fall);
+        $count_second_spring = count($second_spring);
+        $count_third_fall = count($third_fall);
+        $count_third_spring = count($third_spring);
+        $count_fourth_fall = count($fourth_fall);
+        $count_fourth_spring = count($fourth_spring);
 
         $lastrow = 0;
+
+    
       
 
         // Create new Spreadsheet object
@@ -149,22 +164,195 @@ class CPanelController extends Controller
         }
 
         $lastrow++;
-  
-                 //9                   //6                 //9
-        for($i = $lastrow; $i< 16; $i++){
-            
-            $c= ($i - $lastrow)-1;
-            
- 
+        $c = $lastrow+1;
+
+        $lastrow_first_spring = 0;
+                 //9                 //6             //9
+        for($i = $lastrow; $i< $count_first_spring + $lastrow ;$i++){
+           
             // Add some data
         $spreadsheet->setActiveSheetIndex(0)
         ->setCellValue('A'.$lastrow , '1st Year - Spring Semester')
-        ->setCellValue('A'.$i, $first_spring[$c]->id);
-            
+        ->setCellValue('A'.$c, $first_spring[$i-9]->id)
+        ->setCellValue('B'.$c, $first_spring[$i-9]->code)
+        ->setCellValue('C'.$c, $first_spring[$i-9]->course)
+        ->setCellValue('D'.$c, $first_spring[$i-9]->weeklyhours)
+        ->setCellValue('E'.$c, $first_spring[$i-9]->ects)
+        ->setCellValue('F'.$c, $first_spring[$i-9]->professor)
+        ->setCellValue('G'.$c, $first_spring[$i-9]->assistant)
+        ->setCellValue('H'.$c, $first_spring[$i-9]->students)
+        ->setCellValue('I'.$c, $first_spring[$i-9]->comment);
+
+        $c++;
+        
+        $lastrow_first_spring = $i;
+
+        }
+
+        //second year fall
+        $lastrow_first_spring += 2;
+        $c = $lastrow_first_spring+1;
+
+        $lastrow_second_fall = 0;
+                 //16                 
+        for($i = $lastrow_first_spring; $i< $count_second_fall + $lastrow_first_spring ;$i++){
+           
+            // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_first_spring , '2nd Year - Fall Semester')
+        ->setCellValue('A'.$c, $second_fall[$i-16]->id)
+        ->setCellValue('B'.$c, $second_fall[$i-16]->code)
+        ->setCellValue('C'.$c, $second_fall[$i-16]->course)
+        ->setCellValue('D'.$c, $second_fall[$i-16]->weeklyhours)
+        ->setCellValue('E'.$c, $second_fall[$i-16]->ects)
+        ->setCellValue('F'.$c, $second_fall[$i-16]->professor)
+        ->setCellValue('G'.$c, $second_fall[$i-16]->assistant)
+        ->setCellValue('H'.$c, $second_fall[$i-16]->students)
+        ->setCellValue('I'.$c, $second_fall[$i-16]->comment);
+
+        $c++;
+        
+        $lastrow_second_fall = $i;
+
+        }
+
+        //second year spring
+        $lastrow_second_fall += 2;
+        $c = $lastrow_second_fall+1;
+
+        $lastrow_second_spring = 0;
+                 //23                 
+        for($i = $lastrow_second_fall; $i< $count_second_spring + $lastrow_second_fall ;$i++){
+           
+            // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_second_fall , '2nd Year - Spring Semester')
+        ->setCellValue('A'.$c, $second_spring[$i-23]->id)
+        ->setCellValue('B'.$c, $second_spring[$i-23]->code)
+        ->setCellValue('C'.$c, $second_spring[$i-23]->course)
+        ->setCellValue('D'.$c, $second_spring[$i-23]->weeklyhours)
+        ->setCellValue('E'.$c, $second_spring[$i-23]->ects)
+        ->setCellValue('F'.$c, $second_spring[$i-23]->professor)
+        ->setCellValue('G'.$c, $second_spring[$i-23]->assistant)
+        ->setCellValue('H'.$c, $second_spring[$i-23]->students)
+        ->setCellValue('I'.$c, $second_spring[$i-23]->comment);
+
+        $c++;
+        
+        $lastrow_second_spring = $i;
+
+        }
+
+        //third fall
+        $lastrow_second_spring += 2;
+        $c = $lastrow_second_spring+1;
+
+        $lastrow_third_fall = 0;
+        //30                 
+        for($i = $lastrow_second_spring; $i< $count_third_fall + $lastrow_second_spring ;$i++){
+           
+            // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_second_spring , '3rd Year - Fall Semester')
+        ->setCellValue('A'.$c, $third_fall[$i-30]->id)
+        ->setCellValue('B'.$c, $third_fall[$i-30]->code)
+        ->setCellValue('C'.$c, $third_fall[$i-30]->course)
+        ->setCellValue('D'.$c, $third_fall[$i-30]->weeklyhours)
+        ->setCellValue('E'.$c, $third_fall[$i-30]->ects)
+        ->setCellValue('F'.$c, $third_fall[$i-30]->professor)
+        ->setCellValue('G'.$c, $third_fall[$i-30]->assistant)
+        ->setCellValue('H'.$c, $third_fall[$i-30]->students)
+        ->setCellValue('I'.$c, $third_fall[$i-30]->comment);
+
+        $c++;
+        
+        $lastrow_third_fall = $i;
+
+        }
+
+        //third spring
+        $lastrow_third_fall += 2;
+        $c = $lastrow_third_fall+1;
+
+        $lastrow_third_spring = 0;
+        //37                 
+        for($i = $lastrow_third_fall; $i< $count_third_spring + $lastrow_third_fall ;$i++){
+           
+            // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_third_fall , '3rd Year - Spring Semester')
+        ->setCellValue('A'.$c, $third_spring[$i-37]->id)
+        ->setCellValue('B'.$c, $third_spring[$i-37]->code)
+        ->setCellValue('C'.$c, $third_spring[$i-37]->course)
+        ->setCellValue('D'.$c, $third_spring[$i-37]->weeklyhours)
+        ->setCellValue('E'.$c, $third_spring[$i-37]->ects)
+        ->setCellValue('F'.$c, $third_spring[$i-37]->professor)
+        ->setCellValue('G'.$c, $third_spring[$i-37]->assistant)
+        ->setCellValue('H'.$c, $third_spring[$i-37]->students)
+        ->setCellValue('I'.$c, $third_spring[$i-37]->comment);
+
+        $c++;
+        
+        $lastrow_third_spring = $i;
+
+        }
+
+        //fourth fall
+        $lastrow_third_spring += 2;
+        $c = $lastrow_third_spring+1;
+
+        $lastrow_fourth_fall = 0;
+        //44                 
+        for($i = $lastrow_third_spring; $i< $count_fourth_fall + $lastrow_third_spring ;$i++){
+           
+        // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_third_spring , '4th Year - Fall Semester')
+        ->setCellValue('A'.$c, $fourth_fall[$i-44]->id)
+        ->setCellValue('B'.$c, $fourth_fall[$i-44]->code)
+        ->setCellValue('C'.$c, $fourth_fall[$i-44]->course)
+        ->setCellValue('D'.$c, $fourth_fall[$i-44]->weeklyhours)
+        ->setCellValue('E'.$c, $fourth_fall[$i-44]->ects)
+        ->setCellValue('F'.$c, $fourth_fall[$i-44]->professor)
+        ->setCellValue('G'.$c, $fourth_fall[$i-44]->assistant)
+        ->setCellValue('H'.$c, $fourth_fall[$i-44]->students)
+        ->setCellValue('I'.$c, $fourth_fall[$i-44]->comment);
+
+        $c++;
+        
+        $lastrow_fourth_fall = $i;
+
+        }
+
+        //fourth spring
+        $lastrow_fourth_fall += 2;
+        $c = $lastrow_fourth_fall+1;
+
+        $lastrow_fourth_spring = 0;
+        //51              
+        for($i = $lastrow_fourth_fall; $i< $count_fourth_spring + $lastrow_fourth_fall ;$i++){
+           
+        // Add some data
+        $spreadsheet->setActiveSheetIndex(0)
+        ->setCellValue('A'.$lastrow_fourth_fall , '4th Year - Spring Semester')
+        ->setCellValue('A'.$c, $fourth_spring[$i-51]->id)
+        ->setCellValue('B'.$c, $fourth_spring[$i-51]->code)
+        ->setCellValue('C'.$c, $fourth_spring[$i-51]->course)
+        ->setCellValue('D'.$c, $fourth_spring[$i-51]->weeklyhours)
+        ->setCellValue('E'.$c, $fourth_spring[$i-51]->ects)
+        ->setCellValue('F'.$c, $fourth_spring[$i-51]->professor)
+        ->setCellValue('G'.$c, $fourth_spring[$i-51]->assistant)
+        ->setCellValue('H'.$c, $fourth_spring[$i-51]->students)
+        ->setCellValue('I'.$c, $fourth_spring[$i-51]->comment);
+
+        $c++;
+        
+        $lastrow_fourth_spring = $i;
+
         }
 
         // Rename worksheet
-        $spreadsheet->getActiveSheet()->setTitle('Simple');
+        $spreadsheet->getActiveSheet()->setTitle('CE Curriculum for 2018/2019');
 
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
         $spreadsheet->setActiveSheetIndex(0);
